7953e6ff800fbc324385a0a9234489744f1fb70d
Araneth25 <allccacogarciaaraneth@gmail.com>
2023-07-02 15:32:26 +0200

    Incluyendo Inicio y Principal

---
+++ Starbucks/app/src/main/java/com/example/starbucks/InicioPrincipalActivity.java
@@ -0,0 +1,14 @@
+package com.example.starbucks;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class InicioPrincipalActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_inicio_principal);
+    }
+}
